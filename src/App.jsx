7244adbe5973d9442a05be3a4d071726e64b9777
IshaniNakarani12 <ishaninakarani12@gmail.com>
2024-06-17 11:52:55 +1000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { ToastContainer } from "react-toastify";
+
+function App() {
+  return (
+    <div>
+      <ToastContainer />
+    </div>
+  );
+}
+
+export default App;
